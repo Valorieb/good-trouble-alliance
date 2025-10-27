ba6eae23b5b0ddac588d73f6ee4f62ddcc2be0a2
Valorieb <valoriebroderick@gmail.com>
2025-10-27 10:35:37 -0500

    Added hero img and updated meeting times

---
+++ scripts/actionbtn.js
@@ -0,0 +1,6 @@
+const actionbtn = document.getElementById("action-button");
+const button = document.createElement("button");
+
+actionbtn.appendChild(button);
+
+button.textContent="Make Good Trouble";
